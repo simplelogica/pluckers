45ba119ef29d5e2a9fe2c3294272184ff4fccd5f
David J. Brenes <davidjbrenes@gmail.com>
2016-09-04 19:12:12 +0200

    Testing belongs_to reflection with customized primary key
    We were searching always by id. Now we use the primary key configured.

--- test/dummy/models/blog_post.rb
+++ test/dummy/models/blog_post.rb
@@ -7,6 +7,7 @@ ActiveRecord::Schema.define do
     t.integer :author_id
     t.integer :editor_id
     t.integer :reviewed_by_id
+    t.string :main_category_title
   end
 
   create_table :blog_post_translations, force: true do |t|
@@ -20,6 +21,7 @@ class BlogPost < ActiveRecord::Base
   belongs_to :author
   belongs_to :editor, class_name: 'Author'
   belongs_to :reviewer, class_name: 'Author', foreign_key: 'reviewed_by_id'
+  belongs_to :main_category, class_name: 'Category', foreign_key: 'main_category_title', primary_key: 'title'
   has_one :user, through: :author
   has_many :references
   has_and_belongs_to_many :categories
--- test/pluckers/belongs_to_test.rb
+++ test/pluckers/belongs_to_test.rb
@@ -28,20 +28,43 @@ class BelongsToTest < Minitest::Test
 
     Author.create! @author_data
 
+    @category_data = [
+      {
+        title: 'Category 1',
+        image: 'image 1'
+      },
+      {
+        title: 'Category 2',
+        image: 'image 2'
+      },
+      {
+        title: 'Category 3',
+        image: 'image 3'
+      },
+      {
+        title: 'Category 4',
+        image: 'image 4'
+      }
+    ]
+
+    categories = Category.create! @category_data
+
     @post_data = [
       {
         title: 'Test title 1',
         text: 'Test text 1',
         author_id: Author.find_by(name: 'Author 1').id,
         editor_id: Author.find_by(name: 'Author 2').id,
-        reviewed_by_id: Author.find_by(name: 'Reviewer 2').id
+        reviewed_by_id: Author.find_by(name: 'Reviewer 2').id,
+        main_category: categories.sample
       },
       {
         title: 'Test title 2',
         text: 'Test text 2',
         author_id: Author.find_by(name: 'Author 2').id,
         editor_id: Author.find_by(name: 'Author 1').id,
-        reviewed_by_id: Author.find_by(name: 'Reviewer 1').id
+        reviewed_by_id: Author.find_by(name: 'Reviewer 1').id,
+        main_category: categories.sample
       },
       {
         title: 'Test title 3',
@@ -54,6 +77,7 @@ class BelongsToTest < Minitest::Test
   def teardown
     Author.delete_all
     BlogPost.delete_all
+    Category.delete_all
   end
 
   def test_it_fetches_all_simple_attributes
@@ -67,6 +91,7 @@ class BelongsToTest < Minitest::Test
         author_id: p.author_id,
         editor_id: p.editor_id,
         reviewed_by_id: p.reviewed_by_id,
+        main_category_title: p.main_category_title,
         author: p.author.nil? ? nil : {
           id: p.author.id,
           name: p.author.name,
@@ -88,6 +113,7 @@ class BelongsToTest < Minitest::Test
         author_id: p.author_id,
         editor_id: p.editor_id,
         reviewed_by_id: p.reviewed_by_id,
+        main_category_title: p.main_category_title,
         editor: p.editor.nil? ? nil : {
           id: p.editor.id,
           name: p.editor.name,
@@ -109,10 +135,33 @@ class BelongsToTest < Minitest::Test
         author_id: p.author_id,
         editor_id: p.editor_id,
         reviewed_by_id: p.reviewed_by_id,
+        main_category_title: p.main_category_title,
         reviewer: p.reviewer.nil? ? nil : {
           id: p.reviewer.id,
           name: p.reviewer.name,
           email: p.reviewer.email
+        }
+      }
+    }
+
+  end
+
+  def test_it_fetches_relationship_with_customized_primary_key
+    @subject = Pluckers::Base.new(BlogPost.all, reflections: { main_category: {} })
+
+    must pluck Proc.new {|p|
+      {
+        id: p.id,
+        title: p.title,
+        text: p.text,
+        author_id: p.author_id,
+        editor_id: p.editor_id,
+        reviewed_by_id: p.reviewed_by_id,
+        main_category_title: p.main_category_title,
+        main_category: p.main_category.nil? ? nil : {
+          id: p.main_category.id,
+          title: p.main_category.title,
+          image: p.main_category.image
         }
       }
     }
@@ -130,6 +179,7 @@ class BelongsToTest < Minitest::Test
         author_id: p.author_id,
         editor_id: p.editor_id,
         reviewed_by_id: p.reviewed_by_id,
+        main_category_title: p.main_category_title,
         author: p.author.nil? ? nil : {
           id: p.author.id,
           name: p.author.name
@@ -150,6 +200,7 @@ class BelongsToTest < Minitest::Test
         author_id: p.author_id,
         editor_id: p.editor_id,
         reviewed_by_id: p.reviewed_by_id,
+        main_category_title: p.main_category_title,
         post_author: p.author.nil? ? nil : {
           id: p.author.id,
           name: p.author.name
